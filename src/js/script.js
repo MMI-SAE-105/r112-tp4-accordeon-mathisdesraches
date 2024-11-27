55acfc3f673b57a07d00138434f6d899e78be49d
mathisdesraches <mathis.desraches@edu.univ-fcomte.fr>
2024-11-27 09:52:24 +0100

    js

--- src/js/script.js
+++ src/js/script.js
@@ -2,3 +2,18 @@
 
 accordeon = document.querySelector(".accordeon");
 accordeon.querySelectorAll("details").forEach((det) => (det.open = false));
+    const accordions = document.querySelectorAll('.accordeon details');
+  
+    accordions.forEach((detail) => {
+      // Ajoute un écouteur d'événements pour chaque élément details
+      detail.addEventListener('click', () => {
+        // Lorsque l'utilisateur clique, fermer tous les autres éléments
+        accordions.forEach((otherDetail) => {
+          if (otherDetail !== detail) {
+            otherDetail.removeAttribute('open');
+          }
+        });
+      });
+    });
+  });
+  
